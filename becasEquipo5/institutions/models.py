1b6fb3d01d87f338b108e427f1cbf483f63547da
ada_hack215 <ada_hack215@hackademy.mx>
2022-08-07 05:52:13 -0500

    Agregados modelo y serializer de Institution
    Creada la tabla institutions con las primeras migraciones

--- becasEquipo5/institutions/models.py
+++ becasEquipo5/institutions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Institution(models.Model):
+    # Atributos
+    name = models.CharField(max_length=120, verbose_name='Nombre Institución')
+    kind = models.CharField(max_length=56, verbose_name='Tipo de Institución')
+    area = models.CharField(max_length=56, verbose_name='Área a la que pertenece')
+    country = models.CharField(max_length=56, verbose_name='País')
+    website = models.CharField(max_length=56, verbose_name='Sitio Web')
+    description = models.CharField(max_length=256, verbose_name='Descripción de la Institución')
+
+    #Nombre de la tabla:
+    class Meta:
+        db_table = 'institutions'
+    
+    #Función que imprime 'name'
+    def __str__(self):
+        return self.name
