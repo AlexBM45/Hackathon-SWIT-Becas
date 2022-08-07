845e28c07181c4236d0488ec6e6437fd96a16e9b
ada_hack215 <ada_hack215@hackademy.mx>
2022-08-07 06:37:01 -0500

    Agredado modelo y serializer de ContactPerson
    Actualizado modelo y serializer de Institution (ForeignKey)

--- becasEquipo5/institutions/serializers.py
+++ becasEquipo5/institutions/serializers.py
@@ -1,8 +1,13 @@
+from dataclasses import field
 from rest_framework import serializers
-from institutions.models import Institution
+from institutions.models import Institution,ContactPerson
+
+class ContactPersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactPerson
+        fields = '__all__'
 
 class InstitutionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Institution
         fields = '__all__'
