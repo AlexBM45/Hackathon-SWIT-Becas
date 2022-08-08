4f5ebd3e864369e9ebe25b2aeb8c59e23dbca305
ada_hack215 <ada_hack215@hackademy.mx>
2022-08-08 01:06:51 -0500

    Se modificaron archivos de la app scholarships

--- becasEquipo5/scholarships/urls.py
+++ becasEquipo5/scholarships/urls.py
@@ -3,5 +3,7 @@ from scholarships import views
 
 urlpatterns = [
     path('', views.ScholarshipView.as_view()),
-    path('requirement/', views.RequirementView.as_view()),
+    path('requirements/', views.RequirementView.as_view()),
+    path('<int:id>', views.ScholarshipSingleView.as_view()),
+    path('requirements/<int:id>', views.RequirementSingleView.as_view()),
 ]
--- becasEquipo5/scholarships/views.py
+++ becasEquipo5/scholarships/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -19,6 +20,36 @@ class ScholarshipView(APIView):
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
 
+class ScholarshipSingleView(APIView):
+
+    def get(self, request, id):
+        scholarship = Scholarship.objects.filter(id=id).first()
+        if scholarship is None:
+            return Response({'error': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = ScholarshipSerializer(scholarship)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        scholarship = Scholarship.objects.get(id=id)
+        if scholarship is None:
+            return Response({'error': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = ScholarshipSerializer(scholarship , data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        scholarship = Scholarship.objects.get(id=id)
+        if scholarship is None:
+            return Response({'error': 'Bad Request'}, status=status.HTTP_400_BAD_REQUEST)
+        scholarship.delete()
+
+        return Response({'message':'Beca eliminada correctamente'}, status=status.HTTP_204_NO_CONTENT)
+
+#--------------------------------------------------------------------------------------------------------
+
 class RequirementView(APIView):
     def get(self, request):
         requirements = Requirement.objects.all()
@@ -31,3 +62,22 @@ class RequirementView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+class RequirementSingleView(APIView):
+
+    def patch(self, request, id):
+        requirements = Requirement.objects.filter(id=id).first()
+        if requirements is None:
+            return Response({'error': 'Bad request'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = RequirementSerializer(requirements, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        requirements = get_object_or_404(Requirement, id=id)
+        requirements.delete()
+
+        return Response('Requisitos eliminados', status=status.HTTP_204_NO_CONTENT)
