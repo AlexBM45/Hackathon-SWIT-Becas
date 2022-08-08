1e78b7364d6aadb6e2fc62d99bcdaf7ebcbddcb7
AlejandraBM <alejandrabenitezmtz@gmail.com>
2022-08-08 14:13:06 -0500

    final details

--- becasEquipo5/becasEquipo5/urls.py
+++ becasEquipo5/becasEquipo5/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('institutions/', include('institutions.urls')),
     path('students/', include('students.urls')),
     path('scholarships/', include('scholarships.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
