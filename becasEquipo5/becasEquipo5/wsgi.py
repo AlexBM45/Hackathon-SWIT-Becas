c561bdc6c600d9c37080ed5de34d233f6c14b612
AlejandraBM <alejandrabenitezmtz@gmail.com>
2022-08-08 10:03:21 -0500

    config

--- becasEquipo5/becasEquipo5/wsgi.py
+++ becasEquipo5/becasEquipo5/wsgi.py
@@ -11,5 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application  
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "becasEquipo5.settings")  
 application = get_wsgi_application()  
+
 from whitenoise.django import DjangoWhiteNoise  
 application = DjangoWhiteNoise(application)
