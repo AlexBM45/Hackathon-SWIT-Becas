d5828497b28ca9ab5a583715b49b8af808a8f936
AlejandraBM <alejandrabenitezmtz@gmail.com>
2022-08-08 09:56:45 -0500

    config

--- becasEquipo5/becasEquipo5/settings.py
+++ becasEquipo5/becasEquipo5/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os  
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (  
     os.path.join(BASE_DIR, 'static'),
 )
+django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
